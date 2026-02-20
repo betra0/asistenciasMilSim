0ab22c972d82ade895ba15b0a49cf98125bcb632
__Dany__ <tolozadaniel5@gmail.com>
2026-02-20 06:27:56 -0300

    cors provisionales

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,12 @@
 import express from "express";
 import appRoutes from "./routes/app.routes.js";
+import cors from "cors";
 
 const app = express();
+
+app.use(cors({
+  origin: "http://localhost:5173"
+}));
 
 app.use(express.json());
 
