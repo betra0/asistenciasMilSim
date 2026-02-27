d4638d8aebf78a7726c34eee3e5d40df347a606a
__Dany__ <tolozadaniel5@gmail.com>
2026-02-27 13:44:13 -0300

    add in getBootstrap stats

--- backend/src/services/bootstrap.service.js
+++ backend/src/services/bootstrap.service.js
@@ -4,21 +4,57 @@ export async function getBootstrap() {
     const client = await pool.connect();
     try {
         const [members, ranks, events] = await Promise.all([
-            client.query("SELECT id, nickname, rank_id, join_date FROM members"),
+            client.query(`
+                SELECT 
+                    m.id, 
+                    m.nickname, 
+                    m.rank_id, 
+                    m.join_date,
+                    s.consecutive_absences,
+                    s.events_attended,
+                    s.total_present,
+                    s.total_absences,
+                    s.total_justified
+                FROM members m
+                LEFT JOIN events_member_stats s
+                ON s.member_id = m.id;
+                
+            `),
             client.query("SELECT id, rank_name FROM ranks"),
             client.query("SELECT id, name, event_date FROM events")
         ]);
         
+        const rankMap = {};
+        for (const rank of ranks.rows) {
+            rankMap[rank.id] = rank.rank_name;
+        }
+        // preparar ranks
         const ranksFormat = []
         for (const rank of ranks.rows) {
             ranksFormat.push(rank.rank_name);
         }
-        for (const member of members.rows){
-            member.rank_name = ranks.rows.find(rank => rank.id === member.rank_id)?.rank_name || "Unknown";
+        const formatMembers=[]
+        for (const member of members.rows){  
+            const stats={
+                consecutive_absences:member.consecutive_absences,
+                events_attended:member.events_attended,
+                total_present:member.total_present,
+                total_absences:member.total_absences,
+                total_justified:member.total_justified
 
+            }
+            const newMember ={
+                id: member.id,
+                nickname:member.nickname,
+                rank_id:member.rank_id,
+                rank_name: rankMap[member.rank_id] || "Unknown",
+                join_date:member.join_date,
+                stats:stats
+            }
+            formatMembers.push(newMember)
         }
         const membersForRank = {}
-        for (const member of members.rows) {
+        for (const member of formatMembers) {
             if (!membersForRank[member.rank_name]) {
                 membersForRank[member.rank_name] = [];
             }
@@ -29,7 +65,7 @@ export async function getBootstrap() {
             ranks: ranksFormat,
             membersForRank: membersForRank,
             events: events.rows,
-            members: members.rows
+            members: formatMembers
         };
     } finally {
         client.release();
