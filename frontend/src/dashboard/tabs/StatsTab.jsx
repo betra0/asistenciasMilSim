6d2152176834ce9755655f91613e7589212c26e0
__Dany__ <tolozadaniel5@gmail.com>
2026-02-27 13:44:51 -0300

    Implement profile opening functionality and enhance stats table rendering

--- frontend/src/dashboard/tabs/StatsTab.jsx
+++ frontend/src/dashboard/tabs/StatsTab.jsx
@@ -13,6 +13,9 @@ export default function StatsTab({}) {
   console.log("hola mundo desde stats");
   
 
+  const openProfile = ()=>{
+    // pass
+  }
 
 
   return (
@@ -26,6 +29,71 @@ export default function StatsTab({}) {
             </p>
             <div id="statsContainer">
                 {/* Tablas generadas con JS */}
+
+
+  {dashboardData.ranks.map((cat) => {
+      if (!dashboardData.membersByRank[cat] || dashboardData.membersByRank[cat].length === 0) {
+        return null;
+      }
+
+
+      return (
+        <div key={cat}>
+          <h4>{cat}</h4>
+
+          <table className="stats-table">
+            <thead>
+              <tr>
+                <th>Nombre</th>
+                <th>Pres.</th>
+                <th>Aus.</th>
+                <th>Avisos</th>
+                <th>Total %</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              {dashboardData.membersByRank[cat].map((m) => {
+                let p = m.stats.total_present || 0,
+                  a = m.stats.total_absences || 0,
+                  j = m.stats.total_justified || 0
+                // sacar los stats de cada mismbro, Pendiente
+
+                const total = p + a + j;
+                const pct = total > 0 ? Math.round((p / total) * 100) : 0;
+
+                let color = "var(--danger-color)";
+                if (pct >= 80) color = "var(--success-color)";
+                else if (pct >= 50) color = "var(--warning-color)";
+
+                return (
+                  <tr key={`${m.id} k`}>
+                    <td
+                      className="clickable-name"
+                      onClick={() => openProfile(m, cat)}
+                    >
+                      {m.nickname}
+                    </td>
+                    <td>{p}</td>
+                    <td>{a}</td>
+                    <td>{j}</td>
+                    <td
+                      style={{
+                        color,
+                        fontWeight: "bold",
+                      }}
+                    >
+                      {pct}%
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      );
+    })}
+
             </div>
         </div>
     </div>
